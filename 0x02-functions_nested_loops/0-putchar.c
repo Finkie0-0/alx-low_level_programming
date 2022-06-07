2a74faa67d154ddf2cd4694f663076f33733ef04
Arnold Mdluli <arnoldmdluli00@gmail.com>
2022-06-07 04:55:39 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main-Entry point
+ *
+ * Return: nothing, return null
+ */
+int main(void)
+{
+	_putcher('_');
+	_putcher('p');
+	_putcher('u');
+	_putcher('t');
+	_putcher('c');
+	_putcher('h');
+	_putcher('e');
+	_putcher('r');
+	_putcher('\n');
+
+	return (0);
+}
