8865805a4b53ef12d4f36220e141635ce4a89c24
Arnold Mdluli <arnoldmdluli00@gmail.com>
2022-06-07 05:30:47 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Print_alphabet - print alphabet in lowercase
+ *
+ * Return: Always (0).
+ */
+void print_alphabet(void)
+{
+char alphabet;
+for(alphabet = 'a'; alpjabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('\n');
+}
