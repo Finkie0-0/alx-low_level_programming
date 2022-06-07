f6f4bac51a5aa67374be15ebd1249db4c9161e05
Arnold Mdluli <arnoldmdluli00@gmail.com>
2022-06-07 05:54:34 -0700

    Task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,13 @@ void print_alphabet_x10(void)
 int alphabet;
 int count;
 count = 0;
-while(count<10)
+while (count < 10)
 {
-for (alphabet = 'a';alphabet<= 'z';alphabet++)
+for (alphabet = 'a' ; alphabet<= 'z' ; alphabet++)
 {
 _putchar(alphabet);
 }
-count ++;
+count++;
 _putchar('\n');
 }
 }
