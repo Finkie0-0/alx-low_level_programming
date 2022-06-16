3ec6911dc1b4487237ab1ede87c354a4421db354
Arnold Mdluli <arnoldmdluli00@gmail.com>
2022-06-16 04:19:35 -0700

    Task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int j;
-for (j = 0; j < n && src[j] != '\0'; j++)
-dest[j] =src[j];
-for (; j < n; j++)
-dest[j] = '\0';
+int byteCount;
+
+for (byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
+dest[byteCount] = src[byteCount];
+for (; byteCount < n; byteCount++)
+dest[byteCount] = '\0';
+
 return (dest);
 }
