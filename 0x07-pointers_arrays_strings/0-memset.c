88697a3fa6a1a7a5c64fb6db7309d55190e39783
Arnold Mdluli <arnoldmdluli00@gmail.com>
2022-06-20 08:37:43 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte.
+ * @s - memory area pointer
+ * @b - constant byte set
+ * @n - first bytes of memory
+ * Return: dest
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int i;
+	/*For statement to increment for a position*/
+	for (i = 0, i < n, i++)
+	{
+		/*Moves up position by one*/
+		*(s + i) = b
+	}
+	return (s);
+}
