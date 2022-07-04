ae2e079b568946023819cd208a1ed2c597870c72
Arnold Mdluli <arnoldmdluli00@gmail.com>
2022-07-04 15:48:30 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,29 +1,88 @@
 #include "dog.h"
 
+#include <stdlib.h>
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 /**
- * new_dog - create a new dog entry.
- *
- * @name: name of the new dog entry.
- * @age: age of the new dog entry.
- * @owner: name of the new dog owner.
- *
- * Return: pointer to the new dog details or NULL on function failure.
- */
-
+* new_dog - function
+* @name: name of dog
+* @age: age of new doggo
+* @owner: owner of new doggo
+*
+* Description: function to create a new dog. Store copy of name & owner
+* Return: ptr, null if fail
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	char *nename, *neowner;
+	int lename, leowner;
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	dog_t *doggo = malloc(sizeof(dog_t));
+
+	if (doggo == NULL)
+		return (NULL);
+	_strlen(name);
+	lename = _strlen(name);
+	nename = malloc((lename + 1) * sizeof(char));
+	if (nename == NULL)
 	{
-		free(dog);
+		free(doggo);
 		return (NULL);
 	}
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
+	_strcpy(nename, name);
 
-	return (dog);
-	free(dog);
+	_strlen(owner);
+	leowner = _strlen(owner);
+	neowner = malloc((leowner + 1) * sizeof(char));
+	if (neowner == NULL)
+	{
+		free(nename);
+		free(doggo);
+		return (NULL);
+	}
+	_strcpy(neowner, owner);
+
+	doggo->name = nename;
+	doggo->age = age;
+	doggo->owner = neowner;
+
+	return (doggo);
+}
+
+/**
+* _strlen - function
+* @s: first operand & pointer
+*
+* Description: function that returns the length of a string
+* Return: Always 0
+*/
+int _strlen(char *s)
+{
+	int index = 0;
+
+	while (*s != '\0')
+	{
+		index++;
+		s++;
+	}
+	return (index);
+}
+/**
+* _strcpy - function
+* @src: copy from
+* @dest: copy to
+*
+* Description: copies strng pnted by src to dest with null
+* Return: char
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
