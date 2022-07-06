486fb02967c3fa351c324e8de8779629f291c208
Arnold Mdluli <arnoldmdluli00@gmail.com>
2022-07-06 16:20:17 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,11 @@
 #include "function_pointers.h"
+
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
  *
- * Return: Nothing.
+ * Return: no return.
  */
 void print_name(char *name, void (*f)(char *))
 {
