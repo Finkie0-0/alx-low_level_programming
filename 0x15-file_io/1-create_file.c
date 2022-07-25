951972f425f0a13c13c85f45da815097d37db0d3
Arnold Mdluli <arnoldmdluli00@gmail.com>
2022-07-25 15:22:48 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,13 +8,17 @@
 int create_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
+
 	if (filename == NULL)
 		return (-1);
+
 	o = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (o < 0)
 		return (-1);
+
 	while (text_content && *(text_content + len))
 		len++;
+
 	w = write(o, text_content, len);
 	close(o);
 	if (w < 0)
