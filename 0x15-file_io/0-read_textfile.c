11f0d2cfa5a18be8498eb9f5a93438cb45fd06d8
Arnold Mdluli <arnoldmdluli00@gmail.com>
2022-07-25 14:45:31 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,35 +12,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t wr, rd, c;
 	char *buf;
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-	{
+	if (!filename)
 		return (0);
-	}
+	fd = open(filename,O_RDONLY);
 
-	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
 		return (0);
-	}
-
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 	rd = read(fd, buf, letters);
-	if (rd == -1)
-	{
-		return (0);
-	}
-
 	wr = write(STDOUT_FILENO, buf, rd);
-	if (wr == -1)
-	{
-		return (0);
-	}
 
-	c - close(fd);
-	if (c == -1)
-	{
-		return (0);
-	}
+	close(fd);
 
+	free(buf);
 	return (wr);
 }
